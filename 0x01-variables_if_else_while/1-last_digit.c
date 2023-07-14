d8bee26c3c3eee4e713ab65db8592ab9f60b2c8a
stanleywanekeya <Stanleywanekeya123@gmail.com>
2023-07-14 12:27:26 +0300

    Condition operator

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,12 +11,11 @@
 int main(void)
 {
 	int n;
+	int m;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int m;
-
-	m  = n % 10;
+	m = n % 10;
 	if (m > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, m);
 	else if (m < 6 && m != 0)
