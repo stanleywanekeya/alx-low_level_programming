b2c271815242c9816c066b9d01364815afe46c07
stanleywanekeya <Stanleywanekeya123@gmail.com>
2023-07-14 16:04:39 +0300

    Combine digit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,15 +10,15 @@ int main(void)
 	int a;
 	int b;
 
-	for(a = 48; a < 58; a++)
+	for (a = 48; a < 58; a++)
 	{
-		for(b = 49; b < 58; b++)
+		for (b = 49; b < 58; b++)
 		{
 			if (b > a)
 			{
 				putchar(a);
 				putchar(b);
-				if((a != 56) || (b != 57))
+				if ((a != 56) || (b != 57))
 				{
 					putchar(',');
 					putchar(' ');
