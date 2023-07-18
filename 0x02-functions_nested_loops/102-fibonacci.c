ac6ef25350be66c3d0753261ff0cb49afe1ac9b8
stanleywanekeya <Stanleywanekeya123@gmail.com>
2023-07-18 19:07:45 +0300

    Fib

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,12 +7,12 @@
 int main(void)
 {
 	int i;
-	int fib = 0, fib1 = 1, sum = 0;
+	unsigned long fib = 0, fib1 = 1, sum = 0;
 
 	for (i = 0; i < 50; i++)
 	{
 		sum = fib + fib1;
-		printf("%d", sum);
+		printf("%lu", sum);
 		fib = fib1;
 		fib1 = sum;
 		if (sum == 49)
