8cfea67f4c40f0051f4b3f0605a1b31da879c311
stanleywanekeya <Stanleywanekeya123@gmail.com>
2023-07-18 13:06:08 +0300

    Find last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point of modulos function
+ *
+ * @c: input integer
+ *
+ * Return: last digit f integer
+ */
+int print_last_digit(int c)
+{
+	int i = c % 10;
+
+	return (i);
+}
