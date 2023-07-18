f0fd91a5c910e58d9fa280ca108e583a8ad68fd7
stanleywanekeya <Stanleywanekeya123@gmail.com>
2023-07-18 16:27:19 +0300

    Print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,8 +7,14 @@
  */
 void print_alphabet_x10(void)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; i < 10; i++)
-		print_alphabet();
+	{
+		for (j = 97; j < 123; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
 }
