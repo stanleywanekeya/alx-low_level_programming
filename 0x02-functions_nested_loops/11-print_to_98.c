bb3508071eec946eb629b19c5047f48aed9a987f
stanleywanekeya <Stanleywanekeya123@gmail.com>
2023-07-18 15:25:38 +0300

    Add to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints number n to 98
+ *@n: inputs integer n
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+		printf("%d, ", i);
+	printf("\n");
+}
