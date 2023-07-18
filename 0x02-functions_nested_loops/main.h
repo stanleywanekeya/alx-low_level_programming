d1bb8e78fc8472ef661b6ae8cc1fab38b24db401
stanleywanekeya <Stanleywanekeya123@gmail.com>
2023-07-18 14:02:55 +0300

    Header

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <unistd.h>
 
 int _putchar(char c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
 #endif
