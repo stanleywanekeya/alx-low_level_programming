7339b0d337e39c52744cd374fb2ce922a1168aa3
stanleywanekeya <Stanleywanekeya123@gmail.com>
2023-07-18 16:35:07 +0300

    Find if lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,14 @@
-#include "lower.h"
+#include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: value 0 (Success)
+ * _islower - Entry point
+ *@c: integer value
+ * Return: value 1 for lower 0 otherwise
  */
-int main(void)
+int _islower(int c)
 {
-	int r;
-
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
+	if (c > 97 || c < 123)
+		return (1);
+	else
+		return (0);
 }
