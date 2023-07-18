ae9daf8b5610bbd49c702b03c972a4e12361efd3
stanleywanekeya <Stanleywanekeya123@gmail.com>
2023-07-18 18:31:42 +0300

    Find if alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ * @c: integer type
+ * Return: 1 for lower and 0 for otherwise
+ */
+int _isalpha(int c)
+{
+	if (c > 97 && c < 123 || c > 64 && c < 91)
+		return (1);
+	else
+		return (0);
+}
