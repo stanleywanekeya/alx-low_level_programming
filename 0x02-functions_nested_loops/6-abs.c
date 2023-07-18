fe74ea70e86e045449d7957958cbfc0ff8525783
stanleywanekeya <Stanleywanekeya123@gmail.com>
2023-07-18 12:57:41 +0300

    Absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Function for absolute value
+ *
+ * @c: Input of integers
+ *
+ * Return: absolute values
+ */
+int _abs(int c)
+{
+	if (c >= 0)
+		return (c);
+	else
+		return (c * -1);
+}
