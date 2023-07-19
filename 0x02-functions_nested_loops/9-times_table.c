6f4ea3aa9b97b8a95d5c63e6174bdcc0b1184300
stanleywanekeya <Stanleywanekeya123@gmail.com>
2023-07-19 04:28:37 +0300

    Prnt last digit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,9 +1,14 @@
 #include "main.h"
 
+/**
+ * times_table - Entry point
+ *
+ * */
+
 void times_table(void)
 {
 	int i, j;
-	int k = 48;
+	char k = '0';
 	int l = 0;
 
 	for (i = 0; i < 10; i++)
@@ -11,7 +16,6 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			_putchar(k);
-			/*_putchar(k + l);
 			_putchar(k + l);
 			_putchar(k + l);
 			_putchar(k + l);
@@ -19,7 +23,8 @@ void times_table(void)
 			_putchar(k + l);
 			_putchar(k + l);
 			_putchar(k + l);
-			_putchar(k + l);*/
+			_putchar(k + l);
+			_putchar(k + l);
 		}
 		++l;
 		_putchar('\n');
