b27110d3c33bf19056edfc9845885c7481be38f0
stanleywanekeya <Stanleywanekeya123@gmail.com>
2023-07-19 04:41:10 +0300

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,30 +3,34 @@
 /**
  * times_table - Entry point
  *
- * */
+ */
 
 void times_table(void)
 {
-	int i, j;
-	char k = '0';
-	int l = 0;
+	int i, j, k;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			_putchar(k);
-			_putchar(k + l);
-			_putchar(k + l);
-			_putchar(k + l);
-			_putchar(k + l);
-			_putchar(k + l);
-			_putchar(k + l);
-			_putchar(k + l);
-			_putchar(k + l);
-			_putchar(k + l);
+			k = j * 1;
+			if (j == 0)
+				_putchar(k + '0');
+			if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
 		}
-		++l;
 		_putchar('\n');
 	}
 }
