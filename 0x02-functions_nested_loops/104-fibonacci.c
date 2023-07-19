363bdc3049135c0d520bfc752d8c5317f0f8d7ec
stanleywanekeya <Stanleywanekeya123@gmail.com>
2023-07-19 05:29:19 +0300

    Fib display

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: value 0 (Success)
+ */
+int main(void)
+{
+	unsigned long int i, b, a l, b1, b2, a1, a2;
+
+	b = 1;
+	a = 2;
+	l = 1000000000;
+	printf("%lu", b);
+
+	for (i = 1; i < 91; i++)
+	{
+		printf(", %lu", a);
+		a += b;
+		b = a - b;
+	}
+	b1 = (b / l);
+	b2 = (b % l);
+	a1 = (a / l);
+	a2 = (a % l);
+	for (i = 92; i < 99; i++)
+	{
+		printf(", %lu", a1 + (a1 / l));
+		printf("%lu", a2 % l);
+		a1 = a1 + b1;
+		b1 = a1 - b1;
+		a2 = a2 + b2;
+		b2 = a2 - b2;
+	}
+	printf("\n");
+	return (0);
+}
