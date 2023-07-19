af54447165f2023c0ee966277ca045001231c95c
stanleywanekeya <Stanleywanekeya123@gmail.com>
2023-07-19 08:17:13 +0300

    Ispositive

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - Entry point
+ * @i: Integer input
+ */
+void positive_or_negative(int i)
+{
+	if (i >= 0)
+		printf("%d is positive\n");
+	else
+		printf("%d is negative\n");
+}
