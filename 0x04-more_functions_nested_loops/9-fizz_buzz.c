5cfc6910e3eefc20c9124fc10a748cab4e82c61c
stanleywanekeya <Stanleywanekeya123@gmail.com>
2023-07-20 14:38:43 +0300

    Fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,14 +11,15 @@ int main(void)
 	for (i = 1; i <= 100; ++i)
 	{
 		if (i % 3 == 0)
-			printf("Fizz");
+			printf(" Fizz");
 		else if (i % 5 == 0)
-			printf("Buzz");
+			printf(" Buzz");
 		else if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz");
-		else
+			printf(" FizzBuzz");
+		else if (i == 1)
 			printf("%d", i);
-		printf(" ");
+		else
+			printf(" %d", i);
 	}
 	printf("\n");
 	return (0);
