41c45ddf7088b9a346d878e0d0044f3b49b5074e
stanleywanekeya <Stanleywanekeya123@gmail.com>
2023-07-24 20:29:06 +0300

    Reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,11 +6,17 @@
  */
 void rev_string(char *s)
 {
-	int i, len = 0;
+	char rev = s[0];
+	int counter = 0;
+	int i;
 
-	for (i = 0; s[i] != '\0'; ++i)
-		++len;
-	/*for (i = len; i >= 0; --i)
-		_putchar(s[i]);*/
-	_putchar('\n');
+	while (s[counter] != '\0')
+		counter++;
+	for (i = 0; i < counter; i++)
+	{
+		counter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
+	}
 }
