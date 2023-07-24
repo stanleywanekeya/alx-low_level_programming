9d0cdb4d3c2bbdec0b4764d0d8d2e84946edabf3
stanleywanekeya <Stanleywanekeya123@gmail.com>
2023-07-24 20:45:00 +0300

    Convert char to int

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,7 +8,14 @@
 
 int _atoi(char *s)
 {
-	int i = d = n = len = f = digit = 0;
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
 
 	while (s[len] != '\0')
 		len++;
