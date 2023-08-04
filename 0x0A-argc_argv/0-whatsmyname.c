e958774e3b3a50212fc72701e506dc43fed40f10
stanleywanekeya <Stanleywanekeya123@gmail.com>
2023-08-04 22:11:10 +0300

    Print   program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,8 @@
  * @argv: command line vector
  *Return: value 0 for success
  */
-int main(int argc, char **argv)
+int main(int argc__attribute__((unused)), char **argv)
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", argv);
 	return (0);
 }
