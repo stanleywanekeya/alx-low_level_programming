31f368f541135c107a182c913fc09316aac6feef
stanleywanekeya <Stanleywanekeya123@gmail.com>
2023-08-10 13:47:51 +0300

    Concatnate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,44 +9,30 @@
  * Return: string pointer
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
+
 {
-	char *res;
-	unsigned int len, len1, i, j;
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	len = len1 = i = j = 0;
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	while (s1[len] != '\0')
-	{
-		++len;
-	}
-	while (s2[len1] != '\0')
-	{
-		++len1;
-	}
-	if (n < len1)
-		res = malloc(sizeof(char) + (len + n + 1));
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
 	else
-		res = malloc(sizeof(char) + (len + len1 + 1));
-
-	if (!res)
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (!s)
 		return (NULL);
-	while (i < len)
+	while (i < len1)
 	{
-		res[i] = s1[i];
-		++i;
+		s[i] = s1[i];
+		i++;
 	}
-	while (n < len1 && i < (len + n))
-	{
-		res[i++] = s2[j++];
-	}
-	while (n > len1 && i < (len + len1))
-	{
-		res[i++] = s2[j++];
-	}
-
-	res[i] = '\0';
-	return (res);
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+	s[i] = '\0';
+	return (s);
 }
