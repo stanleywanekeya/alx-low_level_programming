c8398259f1c475241697d13cead6c0f1395584b6
stanleywanekeya <Stanleywanekeya123@gmail.com>
2023-09-04 22:19:58 +0300

    Create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-
+	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
 			len++;
