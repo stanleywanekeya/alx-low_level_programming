807a59479edc91bedc02f8011d977e138c59170b
stanleywanekeya <Stanleywanekeya123@gmail.com>
2023-09-04 22:04:20 +0300

    Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,30 +8,19 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, sz, len;
+	int fd, w, len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-	if (text_content)
-	{
-		for (len = 0; text_content[len]; ++len)
-			;
-	}
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (fd == -1)
-		return (-1);
-	if (!text_content)
-	{
-		close(fd);
-		return (0);
-	}
-	else
-	{
-		sz = write(fd, text_content, len);
-		if (sz == -1)
-			return (-1);
-	}
 
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+	if (fd == -1 || w == -1)
+		return (-1);
 	close(fd);
 	return (1);
 }
