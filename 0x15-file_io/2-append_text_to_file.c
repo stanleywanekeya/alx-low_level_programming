10fd40ce029d3d5b9e59496f7f417460d360e199
stanleywanekeya <Stanleywanekeya123@gmail.com>
2023-09-04 21:59:32 +0300

    Append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,21 +8,19 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, len, sz;
+	int sz, w, len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
-			++len;
+			len++;
 	}
-	fd = open(filename, O_WRONLY | O_APPEND);
-	sz = write(fd, text_content, len);
-
-	if (fd == -1 || sz == -1)
+	sz = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+	if (o == -1 || w == -1)
 		return (-1);
-	close(fd);
+	close(sz);
 	return (1);
 }
