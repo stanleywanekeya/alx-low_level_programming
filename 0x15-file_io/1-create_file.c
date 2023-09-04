d500e1556ad5edabdb154148810abd1333ce9088
stanleywanekeya <Stanleywanekeya123@gmail.com>
2023-09-04 21:51:48 +0300

    Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len]; ++len)
 			;
 	}
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	if (!text_content)
